526e1f38953ad5337bd38620a8f2a3ae20c45c47
rayhanebousnina <bousninarayhanesimplonenda@gmail.com>
2021-04-14 02:40:11 +0200

    third commit

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -1,13 +1,15 @@
 var icon = document.getElementById("icon-dark-mode");
 var logo = document.getElementById("logo");
+var logoFooter = document.getElementById("logo-footer");
 icon.onclick = function () {
   document.body.classList.toggle("dark-theme");
   if (document.body.classList.contains("dark-theme")) {
     icon.src = "images/sun.png";
     logo.src = "images/logo-bg-noir.png";
+    logoFooter.src = "images/logo-bg-noir.png";
   } else {
     icon.src = "images/moon.png";
     logo.src = "images/logo-bg-blanc.png";
+    logoFooter.src = "images/logo-bg-blanc.png";
   }
 };
- 
